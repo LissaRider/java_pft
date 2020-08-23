1ad12dd1312ca4ed50b4240b8f9cb0c972b00a88
Vasilisa Smirnova <lissarider@gmail.com>
2020-08-23 16:38:42 +0300

    Задание 2.2 Создана функция для вычисления расстояние между двумя точками

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -18,5 +18,19 @@ public class MyFirstProgram {
     System.out.println("Hello, " + somebody + "!");
   }
 
-
+  // Расстояние между двумя точками p1(x1;y1) и p2(x2;y2) в прямоугольной системе координат выражается формулой:
+  // d = Math.sqrt(Math.pow(x2−x1, 2) + Math.pow(y2−y1, 2)
+  // Метод Math.pow(double base, double exponent) - возведение в степень
+  // Метод Math.sqrt(double base) — возвращает квадратный корень из аргумента
+  public static double distance(Point p1, Point p2) {
+    // Координаты точки p1
+    double x1 = 4;
+    double y1 = 5;
+    // Координаты точки p2
+    double x2 = 8;
+    double y2 = 10;
+    p1 = new Point(x1, y1);
+    p2 = new Point(x2, y2);
+    return Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p2.y, 2));
+  }
 }
