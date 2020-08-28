8d75bc49f496255c6552665ad4f0782f4be8f043
Vasilisa Smirnova <lissarider@gmail.com>
2020-08-29 01:58:29 +0300

    Рефакторинг: выделены локаторы и базовые методы

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -3,12 +3,13 @@ package ru.stqa.pft.addressbook;
 import org.openqa.selenium.By;
 //import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import java.io.File;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class GroupCreationTests {
@@ -17,7 +18,7 @@ public class GroupCreationTests {
 
   @BeforeClass(alwaysRun = true)
   public void setUp() {
-
+    
     driver = new FirefoxDriver();
 
 //    driver = new ChromeDriver();
@@ -29,46 +30,23 @@ public class GroupCreationTests {
     driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     baseUrl = "http://localhost/addressbook";
     driver.get(baseUrl);
-    driver.findElement(By.name("user")).click();
-    driver.findElement(By.name("user")).clear();
-    driver.findElement(By.name("user")).sendKeys("admin");
-
-    driver.findElement(By.name("pass")).click();
-    driver.findElement(By.name("pass")).clear();
-    driver.findElement(By.name("pass")).sendKeys("secret");
-
-    driver.findElement(By.cssSelector("input[type=submit]")).click();
+    login("admin","secret");
   }
+
 
   @Test
   public void testGroupCreation() {
-
-    driver.findElement(By.cssSelector("#nav a[href='group.php']")).click();
-
-//    driver.findElement(By.cssSelector("input[name=new]:nth-child(1)")).click(); /*только верхняя кнопка*/
-//    driver.findElement(By.cssSelector("input[name=new]:nth-child(2)")).click(); /*только нижняя кнопка*/
-//    driver.findElements(By.name("new")).stream().findFirst().get().click(); /*первая кнопка из найденных*/
-    driver.findElements(By.name("new")).stream().findAny().get().click(); /*любая кнопка из найденных*/
-
-    driver.findElement(By.name("group_name")).click();
-    driver.findElement(By.name("group_name")).clear();
-    driver.findElement(By.name("group_name")).sendKeys("Relatives");
-//    driver.findElement(By.name("group_name")).sendKeys("Коллеги");
-//    driver.findElement(By.name("group_name")).sendKeys("Друзья");
-//    driver.findElement(By.name("group_name")).sendKeys("Знакомые");
-
-    driver.findElement(By.name("group_header")).click();
-    driver.findElement(By.name("group_header")).clear();
-    driver.findElement(By.name("group_header")).sendKeys("<h1>RELATIVES</h1><p>Created by Lissa Rider</p></p>");
-
-    driver.findElement(By.name("group_footer")).click();
-    driver.findElement(By.name("group_footer")).clear();
-    driver.findElement(By.name("group_footer")).sendKeys("<a href=\"edit.php\">add contact</a> \n" +
-            " <a href=\"group.php?new=New+group\" target=\"_self\">add group</a>");
-
-    driver.findElement(By.name("submit")).click();
-
-    driver.findElement(By.cssSelector("#content a[href='group.php']")).click();
+    getElement(groupsPageNavLinkLoc).click();
+//    getElement(topAddGroupBtnLoc).click(); /*только верхняя кнопка*/
+//    getElement(bottomAddGroupBtnLoc).click(); /*только нижняя кнопка*/
+//    getFirstElement(addGroupBtnLoc).click(); /*первая кнопка из найденных*/
+    getAnyElement(addGroupBtnLoc).click(); /*любая кнопка из найденных*/
+    clearAndType(groupNameLoc, "Relatives");
+    clearAndType(groupHeaderLoc, "<h1>RELATIVES</h1><p>Created by Lissa Rider</p></p>");
+    clearAndType(groupFooterLoc, "<a href=\"edit.php\">add contact</a>  <a href=\"group.php?new=New+group\" " +
+            "target=\"_self\">add group</a>");
+    getElement(createGroupBtnLoc).click();
+    getElement(returnToGroupsPageBtnLoc).click();
   }
 
   @AfterClass(alwaysRun = true)
@@ -76,7 +54,55 @@ public class GroupCreationTests {
     driver.quit();
   }
 
-//  private boolean isElementPresent(By by) {
+  //region Locators
+  public By passwordLoc = By.name("pass");
+  public By usernameLoc = By.name("user");
+  public By loginBtnLoc = By.cssSelector("input[type=submit]");
+  public By groupsPageNavLinkLoc = By.cssSelector("#nav a[href='group.php']");
+  public By addGroupBtnLoc = By.name("new");
+  //  public By topAddGroupBtnLoc = By.cssSelector("input[name=new]:nth-child(1)");
+//  public By bottomAddGroupBtnLoc = By.cssSelector("input[name=new]:nth-child(2)");
+  public By groupNameLoc = By.name("group_name");
+  public By groupHeaderLoc = By.name("group_header");
+  public By groupFooterLoc = By.name("group_footer");
+  public By createGroupBtnLoc = By.name("submit");
+  public By returnToGroupsPageBtnLoc = By.cssSelector("#content a[href='group.php']");
+  //endregion
+
+  //region Helper methods
+  public void login(String username, String password) {
+    getElement(usernameLoc).click();
+    getElement(usernameLoc).clear();
+    getElement(usernameLoc).sendKeys(username);
+    clearAndType(passwordLoc,password);
+    getElement(loginBtnLoc).click();
+  }
+  //endregion
+
+  // region Base methods
+  public WebElement getElement(By by) {
+    return driver.findElement(by);
+  }
+
+  public List<WebElement> getElements(By by) {
+    return driver.findElements(by);
+  }
+
+  public WebElement getAnyElement(By by) {
+    return getElements(by).stream().findAny().get();
+  }
+
+  public WebElement getFirstElement(By by) {
+    return getElements(by).stream().findFirst().get();
+  }
+
+  public void clearAndType(By by, String value) {
+    getElement(by).click();
+    getElement(by).clear();
+    getElement(by).sendKeys(value);
+  }
+
+//  public boolean isElementPresent(By by) {
 //    try {
 //      driver.findElement(by);
 //      return true;
@@ -84,4 +110,5 @@ public class GroupCreationTests {
 //      return false;
 //    }
 //  }
+  //endregion
 }
