82d14eb678f3b07a865240f4a402bfed21d1cf70
Vasilisa_Smirnova <vssmirnova@vtb.ru>
2020-09-12 17:46:27 +0300

    Реализован выбор максимального элемента с помощью Comparator

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.models.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -22,12 +23,13 @@ public class GroupCreationTests extends TestBase {
     app.group().createGroup(group);
     List<GroupData> after = app.group().getGroupsList();
     Assert.assertEquals(after.size(), before.size() + 1);
-    int max = 0;
-    for (GroupData g : after) {
-      if (g.getId() > max)
-        max = g.getId();
-    }
-    group.setId(max);
+//    int max = 0;
+//    for (GroupData g : after) {
+//      if (g.getId() > max)
+//        max = g.getId();
+//    }
+//    group.setId(max);
+    group.setId(after.stream().max(Comparator.comparingInt(GroupData::getId)).get().getId());
     before.add(group);
     Assert.assertEquals(new HashSet<>(before), new HashSet<>(after));
   }
@@ -44,12 +46,13 @@ public class GroupCreationTests extends TestBase {
     app.group().createGroup(group);
     List<GroupData> after = app.group().getGroupsList();
     Assert.assertEquals(after.size(), before.size() + 1);
-    int max = 0;
-    for (GroupData g : after) {
-      if (g.getId() > max)
-        max = g.getId();
-    }
-    group.setId(max);
+//    int max = 0;
+//    for (GroupData g : after) {
+//      if (g.getId() > max)
+//        max = g.getId();
+//    }
+//    group.setId(max);
+    group.setId(after.stream().max(Comparator.comparingInt(GroupData::getId)).get().getId());
     before.add(group);
     Assert.assertEquals(new HashSet<>(before), new HashSet<>(after));
   }
